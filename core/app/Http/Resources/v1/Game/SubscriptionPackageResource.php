964122621a0d010653f1ff546fc06afe7b045c3f
Momen <mdnurulmomen.bd@gmail.com>
2019-06-25 20:10:22 +0600

    Subscription Package id is added

--- core/app/Http/Resources/v1/Game/SubscriptionPackageResource.php
+++ core/app/Http/Resources/v1/Game/SubscriptionPackageResource.php
@@ -16,6 +16,7 @@ class SubscriptionPackageResource extends JsonResource
     {
         // return parent::toArray($request);
         return [
+            'id'=>$this->id,
             'name'=>$this->name,
             'type'=>$this->subscriptionPackageType->name,
             'time'=>$this->offered_time.' Hours'
