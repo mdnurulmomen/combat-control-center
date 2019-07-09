0c7c0a37ee716e7eeb33facb4b973369e381df73
Momen <mdnurulmomen.bd@gmail.com>
2019-07-09 15:51:45 +0600

    Field 'active' is added to admins table

---
+++ core/database/migrations/2019_01_02_073554_create_admins_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname')->nullable()->default('Full');
+            $table->string('lastname')->nullable()->default('Name');
+            $table->string('username')->default('admin');
+            $table->string('password');
+            $table->boolean('is_verified')->default(false);
+            $table->boolean('active')->default(false);
+            $table->rememberToken();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('picture')->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('country')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+}
