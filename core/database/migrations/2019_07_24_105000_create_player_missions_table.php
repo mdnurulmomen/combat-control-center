db65faeeaf7c483525b1a28a6492c37c54136b6a
Momen <mdnurulmomen.bd@gmail.com>
2019-07-24 11:40:53 +0600

    PlayerMission Model has been added to store players mission

---
+++ core/database/migrations/2019_07_24_105000_create_player_missions_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayerMissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player_missions', function (Blueprint $table) {
+            
+            $table->increments('id');
+
+            $table->mediumInteger('progress_play_number')->nullable()->default(0);
+            $table->mediumInteger('progress_play_time')->nullable()->default(0);
+            $table->mediumInteger('progress_kill_opponent')->nullable()->default(0);
+            $table->mediumInteger('progress_kill_monster')->nullable()->default(0);
+            $table->mediumInteger('progress_win_top_time')->nullable()->default(0);
+            $table->mediumInteger('progress_among_two_time')->nullable()->default(0);
+            $table->mediumInteger('progress_among_three_time')->nullable()->default(0);
+            $table->mediumInteger('progress_among_five_time')->nullable()->default(0);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player_missions');
+    }
+}
