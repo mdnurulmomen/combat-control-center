a2ce1d7bef7d7d57d1b57975a0b6c6cd12c3a601
Momen <mdnurulmomen.bd@gmail.com>
2019-07-29 11:31:02 +0600

    Command Class has been added for cron job

---
+++ core/app/Console/Commands/PlayerTreasureAndSubscription.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\PlayerTreasure;
+use Illuminate\Console\Command;
+use App\Models\PlayerSubscription;
+
+class PlayerTreasureAndSubscription extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'playerTreasureAndSubscirption:day';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Player expired Treasure and Subscription Period are disabled successfully';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        PlayerSubscription::where('end_time','<', now())->update(['status' => 0]);
+        PlayerTreasure::whereMonth('close_time','!=', '0')->whereDate('close_time','<', today())->update(['status' => -2]);
+    }
+}
