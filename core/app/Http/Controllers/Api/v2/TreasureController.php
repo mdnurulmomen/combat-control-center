8648af5838bcd64b2bd51c1d31487abbfb5fdbc1
Momen <mdnurulmomen.bd@gmail.com>
2019-07-30 12:00:00 +0600

    Earning Changed column names are adjusted in v2/TreasureControllers

--- core/app/Http/Controllers/Api/v2/TreasureController.php
+++ core/app/Http/Controllers/Api/v2/TreasureController.php
@@ -27,8 +27,8 @@ class TreasureController extends Controller
 
     public function treasureIdentifier()
     {
-        $updatedEarn = Earning::orderBy('total_earning', 'DESC')->first();
-        $currentEarn = $updatedEarn->current_earning ?? 0;
+        $updatedEarn = Earning::orderBy('total_gems_earning', 'DESC')->first();
+        $currentEarn = $updatedEarn->current_gems_earning ?? 0;
 
         $giftTreasure = GiftTreasure::first();
         $required_earn = $giftTreasure->required_earn ?? 0;
@@ -39,7 +39,7 @@ class TreasureController extends Controller
 
             if ($treasureDetails) {
                 
-                $updatedEarn->decrement('current_earning', $required_earn ?? 0);
+                $updatedEarn->decrement('current_gems_earning', $required_earn ?? 0);
                 return ['giftTreasure'=>'true', 'treasureDetails'=>new TreasureResource($treasureDetails)];
             }
 
