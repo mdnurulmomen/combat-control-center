3c166375c2a0b7157b487075e9ef16dadb9ef3d3
Momen <mdnurulmomen.bd@gmail.com>
2019-07-30 13:08:29 +0600

    Earning Changed column names are adjusted in app.blade

--- core/resources/views/admin/master_layout/app.blade.php
+++ core/resources/views/admin/master_layout/app.blade.php
@@ -48,7 +48,7 @@
       <ul class="app-nav">
         
         <li class="app-search">
-          <input class="app-search__input text-center" type="text" value="💎 {{ App\Models\Earning::latest()->first()->total_earning }}" placeholder="Search" readonly="true">
+          <input class="app-search__input text-center" type="text" value="💎 {{ App\Models\Earning::latest()->first()->total_gems_earning ?? 0}}" placeholder="Search" readonly="true">
         </li>
 
         <!-- User Menu-->
