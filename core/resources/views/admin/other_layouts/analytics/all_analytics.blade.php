97d7eab81634af2f508979f0107faf823c059e52
Momen <mdnurulmomen.bd@gmail.com>
2019-08-05 16:31:11 +0600

    Analytic ui design  is modified

--- core/resources/views/admin/other_layouts/analytics/all_analytics.blade.php
+++ core/resources/views/admin/other_layouts/analytics/all_analytics.blade.php
@@ -2,8 +2,7 @@
 @extends('admin.master_layout.app')
 @section('contents')     
 
-    <div class="row">
-        
+    <div class="row">    
         <div class="col-md-6">
             <div class="tile">
                 <h3 class="tile-title">Talktime</h3>
@@ -103,40 +102,30 @@
                 
             </div>
         </div>
-
     </div>
 
-    <div class="row">
-        
+    <div class="row">   
         <div class="col-md-6">
-            <div class="tile">
-                   
-               <div class="row">
-                   
-                   <div class="col-md-7 col-5">
+            <div class="tile">    
+                <div class="row">   
+                    <div class="col-md-7 col-5">
                         <h3 class="tile-title">Physical Treasure </h3> 
-                   </div>
+                    </div>
 
-                   <div class="col-md-5 col-7 text-right">
-                        
-                        <p class="bg-warning text-center m-0">
-                            # Treasure Gifted : {{ $treasureCounter->total_treasure_gifted }}
+                    <div class="col-md-5 col-7 float-right">    
+                        <p class="bg-info m-0">
+                            # Treasure Gifted : <b>{{ $treasureCounter->total_treasure_gifted }}</b>
                         </p>
                     
-                        <p class="bg-warning text-center m-0">
-                            # Treasure Picked : {{ $treasureCounter->total_treasure_collected }}
-                        </p>    
-                       
+                        <p class="bg-warning m-0">
+                            # Treasure Picked : <b>{{ $treasureCounter->total_treasure_collected }}</b>
+                        </p>       
                    </div>
-
                 </div>
                 
                 <div class="card mb-3 border-dark">
-
                     <div class="card-body">                                     
-
                         <div class="row">
-
                             <div class="col-md-6 mb-4">
                                 <label for="validationServerUsername">Start Date</label>
                                 <div class="input-group">
@@ -150,11 +139,9 @@
                                     <input class="form-control is-valid datePicker treasureTable" type="text" name="treasureEndDate" placeholder="Select Date" autocomplete="off">
                                 </div>
                             </div>
-
                         </div>                        
 
                         <div class="row">
-
                             <div class="col-md-6 text-center">
                                 <div class="widget-small info">
                                     <i class="icon fa fa-stack  ">#</i>
@@ -177,9 +164,7 @@
                                     </div>
                                 </div>
                             </div> 
-
                         </div>
-
                     </div>
                 </div> 
             </div>
@@ -188,12 +173,9 @@
         <div class="col-md-6">
             <div class="tile">
                 <h3 class="tile-title">Gems</h3>
-
                 <div class="card mb-3 border-dark">
                     <div class="card-body">
-
                         <div class="row">
-
                             <div class="col-md-6 mb-4">
                                 <label for="validationServerUsername">Start Date</label>
                                 <div class="input-group">
@@ -207,11 +189,9 @@
                                     <input class="form-control is-valid datePicker gemPacksTable" type="text" name="gemsPackEndDate" placeholder="Select Date" autocomplete="off">
                                 </div>
                             </div>
-
                         </div>                        
 
                         <div class="row">
-
                             <div class="col-md-6 text-center">
                                 <div class="widget-small info">
                                     <i class="icon fa fa-stack  ">#</i>
@@ -239,8 +219,7 @@
                                                         $totalCost += App\Models\Store::find($soldPack->item_id)->offered_price_taka ?? 0;
                                                     }
                                                 }
-
-                                                
+ 
                                             @endphp
 
                                             BDT : <b>{{ $totalCost }}</b> 
@@ -254,11 +233,9 @@
                 </div> 
             </div>
         </div>
-
     </div>
 
-    <div class="row">
-        
+    <div class="row">    
         <div class="col-md-12">
             <div class="tile">
 
@@ -290,15 +267,12 @@
                                 </thead>
                                 
                             </table>
-
+                            
                         </div>
-
                     </div>
                 </div> 
-
             </div>
         </div>
-
     </div>
 
 @stop
